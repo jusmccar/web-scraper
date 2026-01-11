ca00dd28764f368fc88e80299ba74280812a36f6
jusmccar <justinmccartney1@yahoo.com>
2026-01-11 15:21:06 -0800

    Add CLI args check

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+function main() {
+	const cliArgs = process.argv.slice(2)
+
+	if (cliArgs.length < 1) {
+		console.error("Error: at least one argument is required.");
+		process.exit(1);
+	}
+
+	if (cliArgs.length > 1) {
+		console.error("Error: more than one argument is not allowed.");
+		process.exit(1);
+	}
+
+	const baseURL = cliArgs[0];
+
+	console.log(`Starting crawler at: ${baseURL}`);
+	process.exit(0);
+}
+
+main();
