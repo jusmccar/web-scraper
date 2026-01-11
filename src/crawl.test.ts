54b72061df025b943347d559ea054d1504a7ac13
jusmccar <justinmccartney1@yahoo.com>
2026-01-10 23:53:21 -0800

    Add getH1FromHTML and getFirstParagraphFromHTML functions

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,9 +1,30 @@
-import { expect, test } from 'vitest'
-import { normalizeURL } from './crawl'
+import { expect, test } from "vitest";
+import { getFirstParagraphFromHTML, getH1FromHTML, normalizeURL } from "./crawl";
 
-test('normalizeURL', () => {
+test("normalizeURL", () => {
 	expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
 	expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path")
 	expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
 	expect(normalizeURL("http://blog.boot.dev/path")).toBe("blog.boot.dev/path")
-})
+});
+
+test("getH1FromHTML", () => {
+	const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+	const actual = getH1FromHTML(inputBody);
+	const expected = "Test Title";
+	expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML", () => {
+	const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+      </main>
+    </body></html>
+  `;
+	const actual = getFirstParagraphFromHTML(inputBody);
+	const expected = "Main paragraph.";
+	expect(actual).toEqual(expected);
+});
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(urlString: string): string {
 	const url = new URL(urlString);
 	const host = url.hostname;
@@ -8,4 +10,36 @@ export function normalizeURL(urlString: string): string {
 	}
 
 	return `${host}${path}`;
+}
+
+export function getH1FromHTML(html: string): string {
+	const dom = new JSDOM(html);
+	const h1 = dom.window.document.querySelector("h1");
+
+	if (!h1 || !h1.textContent) {
+		return "";
+	}
+
+	return h1.textContent;
+}
+
+export function getFirstParagraphFromHTML(html: string): string {
+	let p: HTMLElement | null = null;
+	const dom = new JSDOM(html);
+	const document = dom.window.document;
+	const main = document.querySelector("main");
+
+	if (main) {
+		p = main.querySelector("p");
+	}
+
+	if (!p) {
+		p = document.querySelector("p");
+	}
+
+	if (!p || !p.textContent) {
+		return "";
+	}
+
+	return p.textContent;
 }
